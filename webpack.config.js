331db6b5991a0caf3b0f9eaba2fd16d213d9d3ae
Tank.vn <tanvd357@gmail.com>
2021-12-31 17:33:46 +0700

    Bundle the library

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');module.exports = {
+	mode: 'production',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'calculator.js',
+		library: 'calculator',
+		libraryTarget: 'umd',
+		globalObject: 'this',
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /(node_modules)/,
+			use: 'babel-loader',
+		}],
+	},
+};
